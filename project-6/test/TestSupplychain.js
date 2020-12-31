ee01070a6b76d0f2910210ea407ef673ad9aa324
Nikos <nikoskordis@gmail.com>
2020-12-31 01:46:01 +0100

    test: a farmer can mark  a coffee item as 'Packed

--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -100,19 +100,20 @@ contract('SupplyChain', function(accounts) {
         const supplyChain = await SupplyChain.deployed()
         
         // Declare and Initialize a variable for event
-        
+        var eventEmitted = false
         
         // Watch the emitted event Packed()
-        
+        await supplyChain.Packed((err, res)=> eventEmitted = true)
 
         // Mark an item as Packed by calling function packItem()
-        
+        await supplyChain.packItem(upc, {from: originFarmerID})
 
         // Retrieve the just now saved item from blockchain by calling function fetchItem()
-        
+        const resultBufferTwo = await supplyChain.fetchItemBufferTwo.call(upc)
 
         // Verify the result set
-        
+        assert.equal(resultBufferTwo[5], 2, 'Error: Invalid item State');
+        assert.equal(eventEmitted, true, 'Packed event not emitted');
     })    
 
     // 4th Test
