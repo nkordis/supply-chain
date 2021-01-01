68fd5d51a2ee9c0efab84bdcb30b81a9c3839bed
Nikos <nikoskordis@gmail.com>
2021-01-01 23:35:47 +0100

    chore: deploy to the Rinkeby Public Network

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,10 +1,30 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+ //const HDWalletProvider = require('@truffle/hdwallet-provider');
+ const infuraKey = "dd6ae1b3765d426d9d26418775516734";
+
+ const fs = require('fs');
+ const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+ 
+  // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
     }
+  },
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
   },
    // Configure your compilers
    compilers: {
